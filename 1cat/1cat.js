afba3b1606f2a8ebd9dc2d0f9523520193223b25
Libor Báňa <108577526+access-excess@users.noreply.github.com>
2026-06-02 02:50:11 +0200

    Create 1cat.js

---
+++ 1cat/1cat.js
@@ -0,0 +1,225 @@
+//  -------------------------------------------
+//    author: https://github.com/access-excess
+//              published: 2-6-2026
+//  license: https://spdx.org/licenses/MIT.html
+//  -------------------------------------------
+
+
+//  tables groups
+
+function mouseEnter0tb() {
+  document.getElementById('cap-trg-tb').style.display="block"
+}
+function mouseLeave0tb() {
+  document.getElementById('cap-trg-tb').style.display="none"
+}
+function Click0tb() {
+  document.getElementById('cap-sqr-tb').style.display="block"
+}
+
+
+function mouseEnter1tb() {
+  document.getElementById('row1-trg-tb').style.display="block"
+}
+function mouseLeave1tb() {
+  document.getElementById('row1-trg-tb').style.display="none"
+}
+function Click1tb() {
+  document.getElementById('row1-sqr-tb').style.display="block"
+  document.getElementById('TB').style.display="none"
+  document.getElementById('OBJ1').style.display="block"
+}
+
+
+function mouseEnter2tb() {
+  document.getElementById('row2-trg-tb').style.display="block"
+}
+function mouseLeave2tb() {
+  document.getElementById('row2-trg-tb').style.display="none"
+}
+function Click2tb() {
+  document.getElementById('row2-sqr-tb').style.display="block"
+  document.getElementById('TB').style.display="none"
+  document.getElementById('OBJ2').style.display="block"
+}
+
+
+function mouseEnter3tb() {
+  document.getElementById('row3-trg-tb').style.display="block"
+}
+function mouseLeave3tb() {
+  document.getElementById('row3-trg-tb').style.display="none"
+}
+function Click3tb() {
+  document.getElementById('row3-sqr-tb').style.display="block"
+  document.getElementById('TB').style.display="none"
+  document.getElementById('OBJ3').style.display="block"
+}
+
+
+
+//  group object 1
+
+function mouseEnter0obj1() {
+  document.getElementById('cap-trg-obj1').style.display="block"
+}
+function mouseLeave0obj1() {
+  document.getElementById('cap-trg-obj1').style.display="none"
+}
+function Click0obj1() {
+  document.getElementById('cap-sqr-obj1').style.display="block"
+  document.getElementById('OBJ1').style.display="none"
+  document.getElementById('TB').style.display="block"
+}
+
+
+function mouseEnter1obj1() {
+  document.getElementById('row1-trg-obj1').style.display="block"
+}
+function mouseLeave1obj1() {
+  document.getElementById('row1-trg-obj1').style.display="none"
+}
+function Click1obj1() {
+  document.getElementById('row1-sqr-obj1').style.display="block"
+}
+
+
+function mouseEnter2obj1() {
+  document.getElementById('row2-trg-obj1').style.display="block"
+}
+function mouseLeave2obj1() {
+  document.getElementById('row2-trg-obj1').style.display="none"
+}
+function Click2obj1() {
+  document.getElementById('row2-sqr-obj1').style.display="block"
+}
+
+
+function mouseEnter3obj1() {
+  document.getElementById('row3-trg-obj1').style.display="block"
+}
+function mouseLeave3obj1() {
+  document.getElementById('row3-trg-obj1').style.display="none"
+}
+function Click3obj1() {
+  document.getElementById('row3-sqr-obj1').style.display="block"
+}
+
+
+function mouseEnter4obj1() {
+  document.getElementById('row4-trg-obj1').style.display="block"
+}
+function mouseLeave4obj1() {
+  document.getElementById('row4-trg-obj1').style.display="none"
+}
+function Click4obj1() {
+  document.getElementById('row4-sqr-obj1').style.display="block"
+}
+
+
+
+//  group object 2
+
+function mouseEnter0obj2() {
+  document.getElementById('cap-trg-obj2').style.display="block"
+}
+function mouseLeave0obj2() {
+  document.getElementById('cap-trg-obj2').style.display="none"
+}
+function Click0obj2() {
+  document.getElementById('cap-sqr-obj2').style.display="block"
+  document.getElementById('OBJ2').style.display="none"
+  document.getElementById('TB').style.display="block"
+}
+
+
+function mouseEnter1obj2() {
+  document.getElementById('row1-trg-obj2').style.display="block"
+}
+function mouseLeave1obj2() {
+  document.getElementById('row1-trg-obj2').style.display="none"
+}
+function Click1obj2() {
+  document.getElementById('row1-sqr-obj2').style.display="block"
+}
+
+
+function mouseEnter2obj2() {
+  document.getElementById('row2-trg-obj2').style.display="block"
+}
+function mouseLeave2obj2() {
+  document.getElementById('row2-trg-obj2').style.display="none"
+}
+function Click2obj2() {
+  document.getElementById('row2-sqr-obj2').style.display="block"
+}
+
+
+function mouseEnter3obj2() {
+  document.getElementById('row3-trg-obj2').style.display="block"
+}
+function mouseLeave3obj2() {
+  document.getElementById('row3-trg-obj2').style.display="none"
+}
+function Click3obj2() {
+  document.getElementById('row3-sqr-obj2').style.display="block"
+}
+
+
+//  group object 3
+
+function mouseEnter0obj3() {
+  document.getElementById('cap-trg-obj3').style.display="block"
+}
+function mouseLeave0obj3() {
+  document.getElementById('cap-trg-obj3').style.display="none"
+}
+function Click0obj3() {
+  document.getElementById('cap-sqr-obj3').style.display="block"
+  document.getElementById('OBJ3').style.display="none"
+  document.getElementById('TB').style.display="block"
+}
+
+
+function mouseEnter1obj3() {
+  document.getElementById('row1-trg-obj3').style.display="block"
+}
+function mouseLeave1obj3() {
+  document.getElementById('row1-trg-obj3').style.display="none"
+}
+function Click1obj3() {
+  document.getElementById('row1-sqr-obj3').style.display="block"
+}
+
+
+function mouseEnter2obj3() {
+  document.getElementById('row2-trg-obj3').style.display="block"
+}
+function mouseLeave2obj3() {
+  document.getElementById('row2-trg-obj3').style.display="none"
+}
+function Click2obj3() {
+  document.getElementById('row2-sqr-obj3').style.display="block"
+}
+
+
+function mouseEnter3obj3() {
+  document.getElementById('row3-trg-obj3').style.display="block"
+}
+function mouseLeave3obj3() {
+  document.getElementById('row3-trg-obj3').style.display="none"
+}
+function Click3obj3() {
+  document.getElementById('row3-sqr-obj3').style.display="block"
+}
+
+
+function mouseEnter4obj3() {
+  document.getElementById('row4-trg-obj3').style.display="block"
+}
+function mouseLeave4obj3() {
+  document.getElementById('row4-trg-obj3').style.display="none"
+}
+function Click4obj3() {
+  document.getElementById('row4-sqr-obj3').style.display="block"
+}
